12f59ff298d4c62414b22cf84b382dcd5043ba69
jukoo <jUmarB@protonmail.com>
2026-04-27 14:19:38 +0000

    minimal script to launch qemu

--- script/qemuefi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-qemu-system-x86_64 -cpu qemu64 -bios /usr/share/edk2/x64/OVMF.fd \
-  -drive file=img/baseufi.disk,if=ide
-
---
+++ script/start_vm.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#- Qemu launch script 
+#- Author  Umar Ba <jUmarB@protonmail.com> 
+#+++++++++++++++++++++++++++++++++++++++++++++++++ 
+set -o pipefail 
+set -o errexit 
+
+[[ -f .config ]] && source .config || {
+  echo -e "Please Launch the Qemu script launcher using start script"
+  exit 1
+} 
+
+
+declare BYTE_ARCH=${ARCH:4}  
+[[ -n  ${BYTE_ARCH} ]]  && { 
+   BYTE_ARCH=x${BYTE_ARCH}  
+}|| BYTE_ARCH="x32"
+
+readonly  BYTE_ARCH 
+readonly  EDK2_FULL_PATH=${EDK2_RPATH}${BYTE_ARCH}/${OVMF_FD} 
+
+has_command(){
+  local exit_code; 
+  [[ -n `command -v $1` ]] || {
+    exit_code=$?
+    printf "command not available :%s\012" ,"${1}"
+    exit ${exit_code} 
+  }
+}
+[[ ! -f ${EDK2_FULL_PATH} ]] && {
+  printf "EFI Development Kit II: [not found]\012"
+  exit -1 
+}
+
+launch_qemu(){
+   qemu-system-${ARCH}  -cpu qemu${ARCH:4} \
+     -bios ${EDK2_FULL_PATH} \
+     -drive file=${IMG_DISK},if=ide \
+     -net none
+}
+
+has_command  qemu-system-${ARCH} 
+launch_qemu  
