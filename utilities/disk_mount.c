6bd3fa10ba9db5e5cf9ebf0e885f2cee85193156
jukoo <jUmarB@protonmail.com>
2026-05-04 20:05:42 +0000

    able to mount image disk

--- utilities/disk_mount.c
+++ utilities/disk_mount.c
@@ -9,7 +9,8 @@
 #include <unistd.h> 
 #include <sys/ioctl.h> 
 #include <linux/loop.h>
-#include <sys/stat.h>
+#include <sys/stat.h> 
+#include <sys/mount.h>
 #include <fcntl.h>
 #include <string.h> 
 #include <errno.h> 
@@ -19,6 +20,8 @@
   EXIT_FAILURE;do{perror(#fcall); fprintf(stderr , __VA_ARGS__);}while(0) 
 
 #define  DEVICE_LOOP_CTRL  "/dev/loop-control" 
+#define  DEVICE_LOOP_NAMING  "/dev/loop"  
+#define  VIRTUAL_FILE_ALLOCATION_TABLE "vfat" 
 
 typedef struct  __loopdev_t loop_dev_t; 
 typedef struct  __loopitem_t loop_it  ; 
@@ -80,7 +83,7 @@ static  int  dm_allocate_loopdev(struct __loopdev_t * restrict  lpdevctr)
      close(lpdevctr->_loopfd) ; 
      return  ~0 ; 
    }
-   asprintf(&lpdevctr->_loopname , "/dev/loop%i", lpdevctr->_devnum) ; 
+   asprintf(&lpdevctr->_loopname , DEVICE_LOOP_NAMING"%i", lpdevctr->_devnum) ; 
    return 0 ;  
 }
 
@@ -111,22 +114,62 @@ static  loop_it *  dm_link(const char *  image_disk ,  const char * devname)
 
 }
 
-static void  dm_auto_remove_allocated_loopdev(struct __loopdev_t * lpdevctr)  
+static int dm_auto_remove_allocated_loopdev(struct __loopdev_t * lpdevctr)  
 { 
 
  
   if(ioctl(lpdevctr->_link->_lo_fd  ,  LOOP_CLR_FD  , lpdevctr->_link->_backing_fd)){
     perr_r(ioctl, "@%s :" , __func__ ,strerror(*__errno_location())) ; 
+    return  -1;    
+  } 
+  
+  if(ioctl(lpdevctr->_loopfd , LOOP_CTL_REMOVE , lpdevctr->_devnum)) 
+  {
+     perr_r(ioctl::LOOP_CTL_REMOVE , "Fail to remove loopdevice \012")  ; 
+     return -1 ; 
   }
- /* 
-  if(-1 == ioctl(lpdevctr->_loopfd, LOOP_CTL_REMOVE , lpdevctr->_devnum)){
-    perr_r(ioctl, "@%s :%s" , __func__ ,strerror(*__errno_location())) ; 
-  }
-   */
+  
+  return 0 ;   
+} 
+
+static int dm_mount(struct  __loopdev_t *  lpdevctr) 
+{
+  if(mount(lpdevctr->_loopname ,"/mnt/efi/" , VIRTUAL_FILE_ALLOCATION_TABLE, 0, 0)) 
+  {
+     perr_r(mount, "Fail to mount this device  :%s\012",strerror(*__errno_location())) ; 
+     return  -1 ; 
+  } 
+
+  return 0;  
+  
 }
+
+static int dm_parse_argument(char * const * av)
+{
+  unsigned int status = 0, 
+               i  = ~0 ; 
+  while(*(av + ++i) || !status ){
+    char *arg =  *(av +i) ; 
+    if(!strlen(arg))
+      continue ; 
+    status ^=((*arg & 0xff) == '-' && (*(arg+1) & 0xff ) == 'u'); 
+     
+  }
+
+  return status ; 
+
+}
+
+static int dm_save_loopdev(struct  __loopdev_t * restrict  lpdevctr)
+{
+  
+}
+
+
 int main(int ac , char *const *av) 
 {
-  unsigned int  pstatus = EXIT_SUCCESS; 
+  unsigned int  pstatus = EXIT_SUCCESS, 
+                has_umount_flag =0 ; 
   const char *fat_image_disk = 00 ; 
 
   loop_dev_t lpdevctr = {0}  ; 
@@ -136,6 +179,10 @@ int main(int ac , char *const *av)
      fprintf(stderr , " Require img disk as entry \012") ;
      goto  _eplg ;
   }
+
+  has_umount_flag = dm_parse_argument(av) ;  
+   
+
   fat_image_disk = *(av+(ac-1)) ; 
 
   //TODO :Add more verification  
@@ -153,8 +200,19 @@ int main(int ac , char *const *av)
     goto  _eplg ; 
   }  
 
-  dm_auto_remove_allocated_loopdev(&lpdevctr) ;  
-  
-_eplg: 
+  if(dm_mount(&lpdevctr)) 
+  {
+    dm_auto_remove_allocated_loopdev(&lpdevctr) ; 
+    goto _eplg ;  
+  }
+
+  //TODO : create  a temporary file to save  the information of the pheripheral  
+ 
+  //dm_save_loopdev(&lpdevctr) ; 
+ 
+  dm_auto_remove_allocated_loopdev(&lpdevctr) ; 
+
+_eplg:
+  dm_emergency_release(&lpdevctr ,  lpdevctr_free) ;  
   return pstatus ;  
 }
